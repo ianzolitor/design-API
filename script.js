768002adb59ef183da8242f7d3860423376f8fe4
Ian Zolitor <ianzolitor@gmail.com>
2017-05-02 16:12:31 -0400

    html formats correctly in body

--- script.js
+++ script.js
@@ -10,7 +10,18 @@ $.ajax({
 		console.log(response)
 
 	response.posts.forEach(function(object){
-            infoSection.innerHTML += "<div class = 'return-box'> <div class = 'return-photo'> <img src='" + object.img + "' width='100%' height='100%'>" +"</div>" + "<div class = 'return-date'>" + object.date + "</div>" + "<div class = 'return-title'>" + object.title +"</div>" + "</div>"
+		var dateFormat = object.date
+		dateFormat = dateFormat.split("/")
+		var month = dateFormat[0]
+		var day = dateFormat[1]
+		if(month === "2"){
+			month = "Feb"
+		}
+		if (month === "1"){
+			month = "Jan"
+		}
+		var newDateFormat = month + " " + day
+            infoSection.innerHTML += "<div class = 'return-box'> <div class = 'return-photo'> <img src='" + object.img + "' width='100%' height='100%'>" +"</div>" + "<div class = 'return-date'>" + newDateFormat + "</div>" + "<div class = 'return-title'>" + object.title +"</div>" + "<hr class = photo-break>" + "<div class = 'return-category'>" + object.categories[0] + "</div>" + "</div>"
 	})
 
 }
